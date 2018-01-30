4c8becce5eff6c8f49b616dedca14ee6da348a96
Ciara Godwin <ciara.godwin3@mail.dcu.ie>
2018-01-30 22:40:20 +0000

    Add basic template

---
+++ algo/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          <li><a href="">Algo</a></li>
+          <li><a href="">Sorting</a></li>
+          <li><a href="">Graphs</a></li>
+          <li><a href="">Data Structures</a></li>
+          <li><a href="">Trees</a></li>
+          <li><a href="">About</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
