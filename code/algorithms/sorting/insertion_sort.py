9af16a277644d1f8aa855e6085596a2247cdb660
Ciara Godwin <ciara.godwin3@mail.dcu.ie>
2018-03-05 12:16:15 +0000

    Fix insertion sort

--- code/algorithms/sorting/insertion_sort.py
+++ code/algorithms/sorting/insertion_sort.py
@@ -2,8 +2,8 @@ def insertion_sort(l):
 	for i in range(len(l)):
 		to_place = l[i]
 		pos = i - 1
-		while 0 < pos and l[pos] > to_place:
+		while 0 <= pos and l[pos] > to_place:
 			l[pos + 1] = l[pos]
 			pos -= 1
-		l[pos] = to_place
+		l[pos + 1] = to_place
 	return l
